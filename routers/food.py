9af282aa8ce41d4ecc64de2bd6ce15eb5402d8be
Joao Paulo <joaopege@gmail.com>
2025-10-04 23:33:40 -0300

    oauth/jwt implemented

--- routers/food.py
+++ routers/food.py
@@ -1,11 +1,11 @@
-from fastapi import Depends, APIRouter, status
-from pydantic import BaseModel
+from fastapi import Depends, APIRouter, status, HTTPException
+from pydantic import BaseModel, Field
 import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
-from models import SessionLocal, FoodEntry
-from pydantic import Field
-from models import User
+from models import SessionLocal, FoodEntry, User
+from .auth import get_current_user
+from typing import Annotated
 
 router = APIRouter()
 Base = declarative_base()
@@ -24,18 +24,31 @@ def get_db():
     finally:
         db.close()
 
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/foods/")
-async def Get_Info_From_Database(db: Session = Depends(lambda: SessionLocal())):
+async def Get_Info_From_Database(user: user_dependency, 
+                                 db: Session = Depends(lambda: SessionLocal())
+                                 ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+    if not db.query(User).filter(User.id == user['id']).first():
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
     food_entries = db.query(FoodEntry).all()
     return food_entries
 
 
 @router.post("/food_entry/", status_code=status.HTTP_201_CREATED)
 async def create_food_entry(
+    user: user_dependency,
     Item: Item,
     db: Session = Depends(lambda: SessionLocal())
 ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+    if not db.query(User).filter(User.id == user['id']).first():
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
     food_entry = FoodEntry(
         user=Item.user,
         food=Item.food,
@@ -51,9 +64,14 @@ async def create_food_entry(
 
 @router.delete("/food_entry/{food_entry_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_food_entry(
+    user: user_dependency,
     food_entry_id: int,
     db: Session = Depends(lambda: SessionLocal())
 ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+    if not db.query(User).filter(User.id == user['id']).first():
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
     food_entry = db.query(FoodEntry).filter(FoodEntry.id == food_entry_id).first()
     if food_entry is None:
         return {"message": "Food entry not found"}
@@ -63,10 +81,15 @@ async def delete_food_entry(
 
 @router.put("/food_entry/{food_entry_id}", status_code=status.HTTP_200_OK)
 async def update_food_entry(
+    user: user_dependency,
     food_entry_id: int,
     Item: Item,
     db: Session = Depends(lambda: SessionLocal())
 ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+    if not db.query(User).filter(User.id == user['id']).first():
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
     food_entry = db.query(FoodEntry).filter(FoodEntry.id == food_entry_id).first()
     if food_entry is None:
         return {"message": "Food entry not found"}
